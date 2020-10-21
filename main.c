8c59a171fac70c2e21e1be51388150c2ddd638e8
cancel <cancel@cancel.fm>
2020-10-21 12:22:24 +0900

    Fix formatting

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int
 main(void) {
-	oso *s = 0;
-	osoputprintf(&s, "I have %d %s", 5, "cucumbers");
-	puts((char *)s);
+  oso *s = 0;
+  osoputprintf(&s, "I have %d %s", 5, "cucumbers");
+  puts((char *)s);
   osocat(&s, " and no carrots");
-	puts((char *)s);
-	osowipe(&s);
-	return 0;
+  puts((char *)s);
+  osowipe(&s);
+  return 0;
 }
